acbf02081544e3da5a375d1dbd8986dcdecc8e50
Ryan McCarron <ryan@Ryans-MacBook-Pro.local>
2025-10-27 15:09:44 +0000

    Adding dotfiles

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+
+# Remove the extra blank line between outputs
+c.TerminalInteractiveShell.separate_in = ''
+c.TerminalInteractiveShell.separate_out = ''
+c.TerminalInteractiveShell.separate_out2 = ''
