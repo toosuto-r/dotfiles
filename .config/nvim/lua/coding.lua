a6ede93689aff61a7be72a3a861b3a35965c4380
Ryan McCarron <ryan@Ryans-MacBook-Pro.local>
2025-11-04 09:56:13 +0000

    Adding cmp

---
+++ .config/nvim/lua/coding.lua
@@ -0,0 +1,30 @@
+return {
+  -- LSP core + installers
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim", config = true },
+  { "williamboman/mason-lspconfig.nvim" },
+
+  -- Completion + sources
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+
+  -- Snippets
+  { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "rafamadriz/friendly-snippets" },
+
+  -- Optional: autopairs that reacts to completion confirm
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({})
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+}
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -13,3 +13,35 @@ local lspconfig = vim.lsp.config('basedpyright', {
 )
 
 vim.lsp.enable('basedpyright')
+
+
+
+
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+
+mason.setup()
+mason_lspconfig.setup({
+  ensure_installed = { "basedpyright" }, -- auto-install
+})
+
+-- Advertise cmp capabilities to LSP so it supplies completion items
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local on_attach = function(_, bufnr)
+  local map = function(mode, lhs, rhs) vim.keymap.set(mode, lhs, rhs, { buffer = bufnr }) end
+  map("n", "gd", vim.lsp.buf.definition)
+  map("n", "K",  vim.lsp.buf.hover)
+  map("n", "<leader>rn", vim.lsp.buf.rename)
+  map("n", "<leader>ca", vim.lsp.buf.code_action)
+end
+
+mason_lspconfig.setup_handlers({
+  function(server)
+    lspconfig[server].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end,
+})
+
